f662fd0a22389a533cdb4d2862aea40173e8172d
brunopapait <brunohpapait@gmail.com>
2023-09-24 22:56:49 -0300

    feat: config class AmazonSimpleEmailService

---
+++ src/main/java/br/com/papait/bruno/emailservice/infra/ses/config/AwsSesConfig.java
@@ -0,0 +1,16 @@
+package br.com.papait.bruno.emailservice.infra.ses.config;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsSesConfig {
+
+  @Bean
+  public AmazonSimpleEmailService amazonSimpleEmailService() {
+    return AmazonSimpleEmailServiceClientBuilder.standard().build();
+  }
+}
